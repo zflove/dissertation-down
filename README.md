3956fbcdd2cef4f0fc3b957ab0efd98158bef368
Xiangyun Huang <xiangyunfaith@outlook.com>
2018-05-11 04:26:25 +0000

    初始化我的项目

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# dissertation-down
+---
+[![Build Status](https://travis-ci.org/XiangyunHuang/dissertation-down.svg?branch=master)](https://travis-ci.org/XiangyunHuang/dissertation-down)
+
+## 本硕博学位论文模板
+
+目标：
+
+- 重构 <https://github.com/XiangyunHuang/master-thesis-template>
+
+- 全部独立的 R Markdown 文件：封面、扉页、独创性声明、授权页、中英文摘要、目录、正文、参考文献、致谢、作者简介、附录
+
+- 定制基于 Pandoc's LaTeX 模板
+
+- 完整的使用说明（假设用户是小白）
+
+- 使用的各方依赖尽可能少
+
+---
+
+特别感谢 Pandoc、TinyTeX、bookdown 的开发者们
---
+++ common.R
@@ -0,0 +1,70 @@
+library(methods)
+set.seed(2018)
+options(digits = 3)
+
+Pkgs <- c(
+  "reticulate",
+  "showtext", "sysfonts", "showtextdb",
+  "fontcm",
+  "tikzDevice", "filehash", "png",
+  "extrafontdb", "Rttf2pt1", "extrafont"
+)
+if (length(setdiff(Pkgs, .packages(TRUE))) > 0) {
+  install.packages(setdiff(Pkgs, .packages(TRUE)))
+}
+
+if (!all(c("source-han-sans-cn", "source-han-serif-cn") %in% showtextdb::font_installed())) {
+  showtextdb::font_install(showtextdb::source_han_serif("CN"))
+  showtextdb::font_install(showtextdb::source_han_sans("CN"))
+}
+extrafont::font_install("fontcm")
+extrafont::loadfonts()
+extrafont::fonts() # registered with pdfFonts()
+sysfonts::font_families() # List available font families
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  echo = TRUE,
+  cache = TRUE,
+  citation.bibtex.max=999,
+  out.width = "70%",
+  fig.align = "center",
+  fig.width = 6,
+  # fig.showtext = TRUE, # too danger
+  fig.asp = 0.618, # 1 / phi
+  # fig.show = "hold", # not for python matplotlib
+  fig.ext = if (knitr::is_html_output()) "svg" else if (knitr::is_latex_output()) "pdf" else "png",
+  dev = if (knitr::is_html_output()) "svg" else if (knitr::is_latex_output()) "pdf" else "png"
+)
+ext <- if (knitr::is_html_output()) ".svg" else if (knitr::is_latex_output()) ".pdf" else ".png"
+
+knitr::knit_hooks$set(optipng = knitr::hook_optipng, pdfcrop = knitr::hook_pdfcrop)
+
+# https://github.com/yihui/knitr-examples/blob/master/085-pdfcrop.Rnw
+# knitr::knit_hooks$set(crop = hook_pdfcrop)
+options(
+  repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN/",
+  tikzDefaultEngine = "xetex",
+  tikzXelatexPackages = c(
+    getOption("tikzXelatexPackages"),
+    "\\usepackage[colorlinks, breaklinks]{hyperref}",
+    "\\usepackage{color}",
+    "\\usepackage{tikz}",
+    "\\usepackage[active,tightpage,xetex]{preview}",
+    "\\PreviewEnvironment{pgfpicture}",
+    "\\usepackage{amsmath}",
+    "\\usepackage{amsfonts}",
+    "\\usepackage{mathrsfs}"
+  )
+)
+# https://github.com/yihui/knitr-examples/blob/master/047-tikz-png.Rnw
+knitr::knit_hooks$set(tikz2png = function(before, options, envir) {
+  # use this hook only for dev='tikz' and externalized tikz graphics
+  if (before || options$dev != "tikz" || !options$external || options$fig.num == 0) return()
+  figs <- knitr:::all_figs(options, ext = "pdf") # all figure names
+  # note the tikz2png option is the extra parameters passed to 'convert'
+  for (fig in figs) {
+    system(sprintf("convert %s %s %s", options$tikz2png, fig, sub("\\.pdf$", ".png", fig)))
+  }
+})
